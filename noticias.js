d12545bf9744c50f8b71247ab8a181305f352217
jcassianojob-hue <jcassianojob@gmail.com>
2026-08-27 23:00:13 -0300

    Add files via upload

---
+++ noticias.js
@@ -0,0 +1,62 @@
+var NOTICIAS = [
+  {
+    tag: "Previdenciário",
+    titulo: "Aposentadoria por idade: veja as regras após a Reforma da Previdência",
+    data: "Jan 2025",
+    link: "blog-tipos-aposentadoria.html"
+  },
+  {
+    tag: "Trabalhista",
+    titulo: "Demissão por justa causa: quando é válida e como contestar",
+    data: "Fev 2025",
+    link: "blog-demissao-justa-causa.html"
+  },
+  {
+    tag: "Previdenciário",
+    titulo: "BPC-LOAS: quem tem direito ao benefício de prestação continuada",
+    data: "Mar 2025",
+    link: "blog-bpc-loas.html"
+  },
+  {
+    tag: "Trabalhista",
+    titulo: "Acidente de trabalho: entenda seus direitos e como o escritório pode ajudar",
+    data: "Abr 2025",
+    link: "blog-acidente-de-trabalho.html"
+  },
+  {
+    tag: "Previdenciário",
+    titulo: "INSS negou seu benefício? Saiba como recorrer com chances reais de êxito",
+    data: "Mai 2025",
+    link: "blog-inss-negou-beneficio.html"
+  },
+  {
+    tag: "Previdenciário",
+    titulo: "Aposentadoria especial: quem trabalha em condições insalubres tem prioridade",
+    data: "Jun 2025",
+    link: "blog-aposentadoria-especial.html"
+  },
+  {
+    tag: "Trabalhista",
+    titulo: "Documentos trabalhistas essenciais para proteger seus direitos",
+    data: "Jul 2025",
+    link: "blog-documentos-trabalhista.html"
+  },
+  {
+    tag: "Trabalhista",
+    titulo: "Horas extras não pagas: como calcular e cobrar o que é seu por direito",
+    data: "Ago 2025",
+    link: "blog.html"
+  },
+  {
+    tag: "Previdenciário",
+    titulo: "Auxílio-doença: como solicitar e quais documentos são necessários",
+    data: "Set 2025",
+    link: "blog.html"
+  },
+  {
+    tag: "Trabalhista",
+    titulo: "Rescisão indireta: quando o empregador é responsável pelo rompimento do contrato",
+    data: "Out 2025",
+    link: "blog.html"
+  }
+];
